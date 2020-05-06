8cdd634f156898010f208a34771d7084152eb075
Wellici Araujo <wja1@ifal.edu.br>
2020-05-06 10:44:40 -0300

    password hashad

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import appointmentsRouters from './appointments.routes';
+import usersRoutes from './users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouters);
+routes.use('/users', usersRoutes);
 
 export default routes;
