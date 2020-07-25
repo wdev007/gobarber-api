f7fafcf6c8ff2ee403359a3d9c78463993fb01db
Wellici Araujo <wja1@ifal.edu.br>
2020-07-25 10:32:59 -0300

    create tests with hash provider fake

--- src/modules/users/services/CreateUserSerive.spec.ts
+++ src/modules/users/services/CreateUserSerive.spec.ts
@@ -1,11 +1,16 @@
 import AppError from '@shared/errors/AppError';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import CreateUserSerivce from './CreateUserService';
+import HashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 
 describe('CreateUser', () => {
   it('should be able to create new user', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
-    const createUserSerivce = new CreateUserSerivce(fakeUsersRepository);
+    const hashProvider = new HashProvider();
+    const createUserSerivce = new CreateUserSerivce(
+      fakeUsersRepository,
+      hashProvider,
+    );
 
     const user = await createUserSerivce.execute({
       name: 'John Doe',
@@ -18,7 +23,11 @@ describe('CreateUser', () => {
 
   it('should not be able to create a new user with same email from another', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
-    const createUserSerivce = new CreateUserSerivce(fakeUsersRepository);
+    const hashProvider = new HashProvider();
+    const createUserSerivce = new CreateUserSerivce(
+      fakeUsersRepository,
+      hashProvider,
+    );
 
     await createUserSerivce.execute({
       name: 'John Doe',
--- src/modules/users/services/SessionsUserService.spec.ts
+++ src/modules/users/services/SessionsUserService.spec.ts
@@ -1,10 +1,54 @@
+import AppError from '@shared/errors/AppError';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import SessionsUserService from './SessionsUserService';
 import CreateUserService from './CreateUserService';
-import HashProvider from '../providers/HashProvider/implementations/BCriptyHashProvider';
+import HashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 
 describe('SessionsUser', () => {
-  it('should be able to create new user', async () => {
+  it('should be able to authenticate with existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const hashProvider = new HashProvider();
+    const createUserSerivce = new CreateUserService(
+      fakeUsersRepository,
+      hashProvider,
+    );
+    const sessionsUserService = new SessionsUserService(
+      fakeUsersRepository,
+      hashProvider,
+    );
+
+    const user = await createUserSerivce.execute({
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: '123456',
+    });
+
+    const response = await sessionsUserService.execute({
+      email: 'johndoe@gmail.com',
+      password: '123456',
+    });
+
+    expect(response).toHaveProperty('token');
+    expect(response.user).toEqual(user);
+  });
+
+  it('should not be able to authenticate with non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const hashProvider = new HashProvider();
+    const sessionsUserService = new SessionsUserService(
+      fakeUsersRepository,
+      hashProvider,
+    );
+
+    expect(
+      sessionsUserService.execute({
+        email: 'johndoe@gmail.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to authenticate with wrong password', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
     const hashProvider = new HashProvider();
     const createUserSerivce = new CreateUserService(
@@ -22,30 +66,11 @@ describe('SessionsUser', () => {
       password: '123456',
     });
 
-    const response = await sessionsUserService.execute({
-      email: 'johndoe@gmail.com',
-      password: '123456',
-    });
-
-    expect(response).toHaveProperty('token');
+    expect(
+      sessionsUserService.execute({
+        email: 'johndoe@gmail.com',
+        password: '12345678',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
-
-  // it('should not be able to create a new user with same email from another', async () => {
-  //   const fakeUsersRepository = new FakeUsersRepository();
-  //   const createUserSerivce = new SessionsUserService(fakeUsersRepository);
-
-  //   await createUserSerivce.execute({
-  //     name: 'John Doe',
-  //     email: 'johndoe@gmail.com',
-  //     password: '123456',
-  //   });
-
-  //   expect(
-  //     createUserSerivce.execute({
-  //       name: 'John Doe 2',
-  //       email: 'johndoe@gmail.com',
-  //       password: '1234567',
-  //     }),
-  //   ).rejects.toBeInstanceOf(AppError);
-  // });
 });
